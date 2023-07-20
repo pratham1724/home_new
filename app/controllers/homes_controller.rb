5958d1016ec968caaf7fe0c60d563e4a98820f83
pratham <prathamparihar21@gmail.com>
2023-07-20 09:17:44 +0530

    first

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,11 @@
 class HomesController < ApplicationController
   def index
-    # if user_signed_in?
-    #   if current_user.client?
-    #     redirect_to trainers_path
-    #   elsif current_user.trainer?
-    #     redirect_to trainer_path(current_user)
-      # end
-    # end
+    if user_signed_in?
+      if current_user.client?
+        redirect_to trainers_path
+      elsif current_user.trainer?
+        redirect_to trainer_path(current_user)
+      end
+    end
   end
 end
